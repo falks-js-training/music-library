dd514299c6d2e12ed709d816e0dcb2c82b9248d5
Falk Hoppe <falk@hey.com>
2023-09-16 16:54:00 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+## usage
+
+```deno
+deno run main.js
+```
---
+++ main.js
@@ -0,0 +1,25 @@
+const artists = ["ABBA", "System Of A Down"];
+
+const albums = {
+  ABBA: [
+    "Album 1",
+    "Album 2",
+    "Album 3",
+  ],
+  "System Of A Down": [
+    "Toxicity",
+    "Steal This Album",
+  ],
+};
+
+// For Schleife: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for
+// console.log(...)
+
+// ABBA:
+//   Album 1
+//   Album 2
+//   Album 3
+
+// System Of A Down:
+//   Toxicity
+//   Steal This Album
